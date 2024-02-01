1baf0d247e9cdd307118a7d621d11b176752203e
Nick McMaster <mcmasternick5572@gmail.com>
2024-02-01 17:50:14 -0500

    half working

--- Assets/Scripts/File/LoadTextFromCSV.cs
+++ Assets/Scripts/File/LoadTextFromCSV.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,10 @@ using Sinbad;
 public class LoadTextFromCSV : MonoBehaviour
 {
     public string fileName;
+    
 
     public List<Line> data = new List<Line>();
+    public List<List<int>> branchIDGroups = new List<List<int>>();
 
 
     // Start is called before the first frame update
@@ -26,9 +29,60 @@ public class LoadTextFromCSV : MonoBehaviour
     {
         data = Sinbad.CsvUtil.LoadObjects<Line>("Assets/RawText/" + file + ".csv");
 
-        int i = 2;
-        Debug.Log(data[i].ID + " " + data[i].TITLE + " " + data[i].BODY);
+        //int i = 2;
+        //Debug.Log(data[i].ID + " " + data[i].TITLE + " " + data[i].BODY);
 
+        
+        branchIDGroups = new List<List<int>>();
+        //check numbers digit by digit WAIT THIS SHOULDNT BE DATA COUNT PLEASE CHANGE THIS FUTURE NICK
+        for(int i = 0; i < data.Count; i++)
+        {
+            int max = -1;
+            bool branchFound = false;
+            //get digits line by line    
+            for(int j = 0; j < data.Count; j++)
+            {
+                string id = data[j].ID;
+                
+                //if the id has the digit we are checking
+                if(i < id.Length)
+                {
+                    //Debug.Log(id.Substring(i, 1));
+                    int x = Int32.Parse(id.Substring(i, 1));
+                    if(x > max)
+                    {
+                        max = x;
+                        branchFound = true;
+                        //Debug.Log("branch found at digit:" + i + " line: " + j);
+                    }
+
+
+                }
+
+            }
+
+            //this is broken or something idk
+            if(branchFound)
+            {
+               for(int k = 0; k < max; k++)
+                {
+                    Debug.Log(Int32.Parse(data[k].ID));
+                    branchIDGroups.Add(new List<int>(Int32.Parse(data[k].ID)));
+                }
+            }
+
+        }
+
+
+        foreach(List<int> list in branchIDGroups)
+        {
+            Debug.Log("branch: ");
+            foreach(int item in list)
+            {
+                Debug.Log(item + " ");
+            }
+            
+        }
 
         
 
